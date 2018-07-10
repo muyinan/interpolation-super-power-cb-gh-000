6da63df11f1f73341e0d002f3783a6937ef25446
Dorna Mu <dorna.mu@gmail.com>
2018-07-10 00:22:33 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def display_rainbow(arrayname)
+  puts "R: #{arrayname[0]}, O: #{arrayname[1]}, Y: #{arrayname[2]}, G: #{arrayname[3]}, B: #{arrayname[4]}, I: #{arrayname[5]}, V: #{arrayname[6]}"
+end
